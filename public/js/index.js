7ff6db5ce025aa761781ecece9010f497b18001f
sumit735 <sumitkumardash1999@gmail.com>
2020-03-27 22:58:06 +0530

    v@1.2.0 .Backend API changed. added support for when data was last updated. and how many individuals are tested till today

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,8 @@ const totalpatients = document.getElementById('totalp');
 const activepatients = document.getElementById('activep');
 const dischargedpatients = document.getElementById('discharged');
 const fatal = document.getElementById('fatal');
+const lastUpdated = document.getElementById('lastUpdated');
+const tested = document.getElementById('tested');
 // daily increased/decreased data
 const increasedTotalpatients = document.getElementById('increasedTotalPatients');
 const increasedActivepatients = document.getElementById('increasedActivePatients');
@@ -10,71 +12,61 @@ const increasedFatal = document.getElementById('increasedFatalPatients');
 // if no data
 const hide = document.getElementById('hide');
 const error = document.querySelector('.error');
-const errorMessage = "Sorry! Something went terribly wrong. Please try again later";
+const errorMessage = 'Sorry! Something went terribly wrong. Please try again later';
 
-fetch('https://api.rootnet.in/covid19-in/stats/latest').then((response) => {
-    response.json().then((data) => {
-        // console.log(data.data);
-        totalpatients.innerHTML = data.data.summary.total;
-        dischargedpatients.innerHTML = data.data.summary.discharged;
-        fatal.innerHTML = data.data.summary.deaths;
-        activepatients.innerHTML = data.data.summary.total - (data.data.summary.discharged + data.data.summary.deaths);
-        fetch('https://api.rootnet.in/covid19-in/stats/daily').then((response) => {
-            response.json().then((dailyData) => {
-                var today = new Date();
-                var dd = String(today.getDate()).padStart(2, '0');
-                var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-                var yyyy = today.getFullYear();
+fetch('https://api.covid19india.org/data.json')
+	.then((response) => {
+		response.json().then((data) => {
+			// console.log(data);
+			totalpatients.innerHTML = data.statewise[0].confirmed;
+			dischargedpatients.innerHTML = data.statewise[0].recovered;
+			fatal.innerHTML = data.statewise[0].deaths;
+			activepatients.innerHTML = data.statewise[0].active;
+			increasedTotalpatients.innerHTML = '(' + data.key_values[0].confirmeddelta + ')';
+			increasedDischargedpatients.innerHTML = '(' + data.key_values[0].recovereddelta + ')';
+			increasedFatal.innerHTML = '(' + data.key_values[0].deceaseddelta + ')';
+			function divideTimeDate(datetime) {
+				// var datetime = data.statewise[0].lastupdatedtime;
 
-                today = yyyy + '-' + mm + '-' + dd;
-                var yesterday = yyyy + '-' + mm + '-' + (parseInt(dd) - 1);
-                console.log(yesterday);
-                console.log(today);
-                console.log(dailyData.data);
-                var count = dailyData.data.length;
-                for (var i = 0; i < count; i++) {
-                    var eachDailyData = dailyData.data[i];
+				var index = datetime.indexOf(' '); // Gets the first index where a space occours
+				return (datetimeData = {
+					date: datetime.substr(0, index), // Gets the first part
+					time: datetime.substr(index + 1) // Gets the text part
+				});
+			}
+			divideTimeDate(data.statewise[0].lastupdatedtime);
+			// console.log(datetimeData);
+			// get today date
+			var today = new Date();
+			var dd = String(today.getDate()).padStart(2, '0');
+			var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+			var yyyy = today.getFullYear();
+			today = dd + '/' + mm + '/' + yyyy;
+			if (datetimeData.date === today) {
+				date = 'today';
+			}
+			function tConvert(time) {
+				// Check correct time format and split into components
+				time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [ time ];
 
-                    // console.log(eachDailyData);
-                    if (eachDailyData.day == yesterday) {
-                        yesterdayData = eachDailyData;
-                    }
-                    if (eachDailyData.day == today) {
-                        todayData = eachDailyData;
-                    }
+				if (time.length > 1) {
+					// If time format correct
+					time = time.slice(1); // Remove full string match value
+					time[5] = +time[0] < 12 ? 'AM' : 'PM'; // Set AM/PM
+					time[0] = +time[0] % 12 || 12; // Adjust hours
+				}
+				return time.join(''); // return adjusted time or original string
+			}
 
-                }
-                // get active patient
-                var yesterdayActivePatient = yesterdayData.summary.total - (yesterdayData.summary.discharged + yesterdayData.summary.deaths);
-                var todayActivePatient = todayData.summary.total - (todayData.summary.discharged + todayData.summary.deaths);
-
-                // get all data
-                var totalPatientIncreased = todayData.summary.total - yesterdayData.summary.total;
-                var activePatientIncreased = todayActivePatient - yesterdayActivePatient;
-                var dischargedPatientIncreased = todayData.summary.discharged - yesterdayData.summary.discharged;
-                var fatalPatientIncreased = todayData.summary.deaths - yesterdayData.summary.deaths;
-
-                // send all data to html
-                const checkPosOrNeg = (number) => {
-                    if (Math.sign(number) === 1) {
-                        return '+' + number;
-                        // console.log('+' + number);
-                    }
-
-                    if (Math.sign(number) === -1) {
-                        return number;
-                        // console.log(number);
-                    }
-                }
-
-                increasedTotalpatients.innerHTML = "(" + checkPosOrNeg(totalPatientIncreased) + ")";
-                increasedActivepatients.innerHTML = "(" + checkPosOrNeg(activePatientIncreased) + ")";
-                increasedDischargedpatients.innerHTML = "(" + checkPosOrNeg(dischargedPatientIncreased) + ")";
-                increasedFatal.innerHTML = "(" + checkPosOrNeg(fatalPatientIncreased) + ")";
-
-            })
-        })
-    })
-}).catch((error) => {
-
-})
+			lastUpdated.innerHTML = 'Last updated on ' + tConvert(datetimeData.time) + ' ' + datetimeData.date;
+			// get tested data
+			var testedData = data.tested[data.tested.length - 1];
+			console.log(testedData);
+			divideTimeDate(testedData.updatetimestamp);
+			tested.innerHTML =
+				'We have tested ' + testedData.totalindividualstested + ' individuals till ' + datetimeData.date;
+		});
+	})
+	.catch((error) => {
+		console.log(error);
+	});
--- public/js/statewise.js
+++ public/js/statewise.js
@@ -2,20 +2,20 @@ const tbody = document.getElementById('data');
 const error = document.querySelector('.error');
 const errorMessage = "Sorry! Something went terribly wrong. Please try again later";
 
-fetch('https://api.rootnet.in/covid19-in/stats/latest').then((response) => {
+fetch('https://api.covid19india.org/data.json').then((response) => {
     response.json().then((data) => {
-        // console.log(data.data.regional);
-        var count = data.data.regional.length;
+        console.log(data.statewise);
+        var count = data.statewise.length;
         temp = "";
-        for (var i = 0; i < count; i++) {
-            var stateData = data.data.regional[i];
+        for (var i = 1; i < count; i++) {
+            var stateData = data.statewise[i];
 
-            // console.log(stateData);
+            console.log(stateData.state);
             //Add the data rows.
             temp += "<tr>";
-            temp += "<td>" + stateData.loc + "</td>";
-            temp += "<td>" + (stateData.confirmedCasesIndian + stateData.confirmedCasesForeign) + "</td>";
-            temp += "<td>" + stateData.discharged + "</td>";
+            temp += "<td>" + stateData.state + "</td>";
+            temp += "<td>" + stateData.active + "</td>";
+            temp += "<td>" + stateData.recovered + "</td>";
             temp += "<td>" + stateData.deaths + "</td>";
             temp += "</tr>";
 
